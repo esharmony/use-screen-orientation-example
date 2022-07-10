e8a0e5243d76b8a624c38c66843b06680b0ca7fe
Matthew Barnden <development.esharmony@gmail.com>
2022-07-10 10:22:33 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useScreenOrientation from 'use-screen-orientation';
 
 function App() {
+  const [orientation, angle] = useScreenOrientation();
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p style={{ background: 'white', color: 'black',  padding: 20 }}> orientation: {orientation}, angle: {angle}</p>
+        </header>
     </div>
   );
 }
